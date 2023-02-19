b17aebf92b7304da6566868153964cc50d832627
jSohnData <jihos@me.com>
2023-02-19 14:05:58 -0500

    FINISHED randomBg

--- src/components/Quiz/QuizCard.js
+++ src/components/Quiz/QuizCard.js
@@ -1,13 +1,7 @@
 import Col from "react-bootstrap/Col";
 
-export default function QuizCard({
-  quizData,
-  questionId,
-  handleAnswer,
-  quizLength,
-}) {
+export default function QuizCard({ quizData, questionId, handleAnswer, quizLength, }) {
   // console.table(quizData)
-
   return (
         <Col md={6} className="quiz-card">
           <h2><span>({questionId + 1}/{quizLength})</span> {quizData[questionId].question}</h2>
--- src/components/Quiz/QuizResult.js
+++ src/components/Quiz/QuizResult.js
@@ -3,9 +3,10 @@ import Col from "react-bootstrap/Col";
 export default function QuizResult ({quizLength, score, handleReset}) {
 
     return (
-        <Col md={6} className="quiz-card">
-            <h2>Quiz Result</h2>
-            <p>You scored {score} out of {quizLength}</p>
+        <Col md={6} className="quiz-card text-center">
+            <h2>Your Result</h2>
+            <p>Thank you for taking the quiz! All quiz materials are from <a href="https://www.javatpoint.com/reactjs-mcq" target="_blank" rel="noreferrer">javatpoint.com/reactjs-mcq</a>.</p>
+            <p>You scored <span className="score">{score}</span> out of {quizLength}.</p>
             <button onClick={handleReset}>Reset</button>
         </Col>
   );    
