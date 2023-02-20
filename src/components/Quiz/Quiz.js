801f15e7e95e284dbe3f72ccb5e89e5debcd4389
jSohnData <jihos@me.com>
2023-02-20 12:04:17 -0500

    CLEANED code

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,53 +1,60 @@
 import { useState } from "react";
 import { Container } from "react-bootstrap";
+
 import Row from "react-bootstrap/Row";
 import quizData from "../../data/quiz.json";
 import QuizCard from "./QuizCard.js";
 import QuizResult from "./QuizResult.js";
-import useBgRandom from "../../hooks/useBgRandom.js"
-import "./quiz.css"
+import useBgRandom from "../../hooks/useBgRandom.js";
+import "./quiz.css";
 
-export default function Quiz () {
-    const[showResult, setShowResult] = useState(false);
-    const[questionId, setQuestionId] = useState(0);
-    const[score, setScore] = useState(0);
-    const quizLength = quizData.length;
-    const handleBgChange = useBgRandom();
+export default function Quiz() {
+  const [showResult, setShowResult] = useState(false);
+  const [questionId, setQuestionId] = useState(0);
+  const [score, setScore] = useState(0);
+  const handleBgChange = useBgRandom();
 
-    const handleAnswer = (isCorrect) => {
-        if (isCorrect === true) setScore(score + 1);
-        console.log({score});
+  const quizLength = quizData.length;
 
-        const newQuestionId = questionId + 1;   
-        
-        handleBgChange();
-                
-        if (newQuestionId < quizLength) {
-            setQuestionId(newQuestionId);
-        } else { 
-            setShowResult(true); }
+  const handleAnswer = (isCorrect) => {
+    if (isCorrect === true) setScore(score + 1);
+    console.log({ score });
+
+    const newQuestionId = questionId + 1;
+
+    handleBgChange();
+
+    if (newQuestionId < quizLength) {
+      setQuestionId(newQuestionId);
+    } else {
+      setShowResult(true);
     }
+  };
 
-    const handleReset = () => {
-        setShowResult(false);
-        setQuestionId(0);
-        setScore(0);
-    }
+  const handleReset = () => {
+    setShowResult(false);
+    setQuestionId(0);
+    setScore(0);
+  };
 
-    return (
-        <Container aria-label="Quiz">
-            <Row className="justify-content-md-center">
-            {(showResult)
-                ? ( <QuizResult
-                        score={score}
-                        quizLength={quizLength}
-                        handleReset={handleReset} /> )
-                : ( <QuizCard 
-                        quizData={quizData}
-                        questionId={questionId}
-                        quizLength={quizLength}
-                        handleAnswer={handleAnswer} /> )}
-            </Row>
-        </Container>        
-    )
+  return (
+    <Container aria-label="Quiz">
+      <Row className="justify-content-md-center">
+        {showResult ? (
+          <QuizResult
+            score={score}
+            quizLength={quizLength}
+            handleReset={handleReset}
+          />
+        ) : (
+          <QuizCard
+            quizData={quizData}
+            questionId={questionId}
+            quizLength={quizLength}
+            handleAnswer={handleAnswer}
+          />
+        )}
+      </Row>
+    </Container>
+  );
 }
